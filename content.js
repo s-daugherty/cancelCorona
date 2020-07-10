5c1d8fd426a1266dba0a1d390c65327091a4c6cc
Anirudh Watturkar <awatturk@mathworks.com>
2020-07-10 17:44:17 -0400

    now with randomization support

--- content.js
+++ content.js
@@ -1,34 +1,68 @@
-
-chrome.storage.local.get(['textInput'], function(result) {
+chrome.storage.local.get(['textQuery'], function (result) {
     var elements = document.getElementsByTagName('*');
-    var s = result.textInput;
-    if (s !== "" && typeof s !== "undefined") {
+    // console.log("txtQ",result.textQuery);
+    if (result.textQuery.isRandom) {
+        // You can add more words here
+        var dictionary = ["pikachu","puppies","Jack Little", "Sunshine", "Reduction in Pollution", "kittens"];
         for (var i = 0; i < elements.length; i++) {
             var element = elements[i];
-        
+
             for (var j = 0; j < element.childNodes.length; j++) {
                 var node = element.childNodes[j];
-        
+                // Picks a random word from dictionary
                 if (node.nodeType === 3) {
                     var text = node.nodeValue;
-                    var replacedText = text.replace(/coronavirus/gi, s);
-                    var replacedText = replacedText.replace(/covid-19/gi, s);
-                    var replacedText = replacedText.replace(/Coronavirus/gi, s);
-                    var replacedText = replacedText.replace(/Covid-19/gi, s);
-                    var replacedText = replacedText.replace(/COVID-19/gi, s);
-        
+                    var replacedText = text.replace(/coronavirus/gi, dictionary[Math.floor(Math.random()*dictionary.length)]);
+                    var replacedText = replacedText.replace(/covid-19/gi, dictionary[Math.floor(Math.random()*dictionary.length)]);
+                    var replacedText = replacedText.replace(/Coronavirus/gi, dictionary[Math.floor(Math.random()*dictionary.length)]);
+                    var replacedText = replacedText.replace(/Covid-19/gi, dictionary[Math.floor(Math.random()*dictionary.length)]);
+                    var replacedText = replacedText.replace(/COVID-19/gi, dictionary[Math.floor(Math.random()*dictionary.length)]);
+                    var replacedText = replacedText.replace(/pandemic/gi, dictionary[Math.floor(Math.random()*dictionary.length)]);
+                    var replacedText = replacedText.replace(/death/gi, dictionary[Math.floor(Math.random()*dictionary.length)]);
+                    var replacedText = replacedText.replace(/disease/gi, dictionary[Math.floor(Math.random()*dictionary.length)]);
+
                     if (replacedText !== text) {
                         element.replaceChild(document.createTextNode(replacedText), node);
                     }
                 }
             }
+        }        
+    } else {
+        var s = result.textQuery.customText;
+        if (s !== "" && typeof s !== "undefined") {
+            for (var i = 0; i < elements.length; i++) {
+                var element = elements[i];
+
+                for (var j = 0; j < element.childNodes.length; j++) {
+                    var node = element.childNodes[j];
+
+                    if (node.nodeType === 3) {
+                        var text = node.nodeValue;
+                        var replacedText = text.replace(/coronavirus/gi, s);
+                        var replacedText = replacedText.replace(/covid-19/gi, s);
+                        var replacedText = replacedText.replace(/Coronavirus/gi, s);
+                        var replacedText = replacedText.replace(/Covid-19/gi, s);
+                        var replacedText = replacedText.replace(/COVID-19/gi, s);
+
+                        if (replacedText !== text) {
+                            element.replaceChild(document.createTextNode(replacedText), node);
+                        }
+                    }
+                }
+            }
         }
-    }    
+    }
 });
 
 let images = document.getElementsByTagName('img');
-for(let i = 0; i < images.length; i++){
-  chrome.runtime.sendMessage({msg: 'image', index: i}, function({data, index}){
-    images[index].src = data.link;
-  });
+for (let i = 0; i < images.length; i++) {
+    chrome.runtime.sendMessage({
+        msg: 'image',
+        index: i
+    }, function ({
+        data,
+        index
+    }) {
+        images[index].src = data.link;
+    });
 }
--- popup.js
+++ popup.js
@@ -1,17 +1,26 @@
 document.getElementById("replace-button").addEventListener("click", saveText);
-
+document.getElementById("randomize-button").addEventListener("click", randomizeText);
 document.getElementById("reset-button").addEventListener("click", resetQuery);
 
 function saveText() {
-        let customText = document.getElementById("wordInput").value;
-        chrome.storage.local.set({textInput: customText});
-        chrome.tabs.getSelected(null, function(tab) {
-            var code = 'window.location.reload();';
-            chrome.tabs.executeScript(tab.id, {code: code});
-        });
+    let customText = document.getElementById("wordInput").value;
+    let isRandom = 0;
+    chrome.storage.local.set({textQuery: {isRandom, customText}});
+    chrome.tabs.getSelected(null, function(tab) {
+        var code = 'window.location.reload();';
+        chrome.tabs.executeScript(tab.id, {code: code});
+    });
 
 }
-
+function randomizeText() {
+    let isRandom = 1;
+    let customText = "";
+    chrome.storage.local.set({textQuery: {isRandom, customText}});
+    chrome.tabs.getSelected(null, function(tab) {
+        var code = 'window.location.reload();';
+        chrome.tabs.executeScript(tab.id, {code: code});
+    });
+}
 function resetQuery() {
     chrome.storage.local.set({textInput: ""});
     chrome.tabs.getSelected(null, function(tab) {
