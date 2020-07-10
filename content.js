3b37d8f2badfb390de5ab31279f552accfbd60be
Anirudh Watturkar <awatturk@mathworks.com>
2020-07-10 15:11:42 -0400

    added auto refresh when setting desired word

--- content.js
+++ content.js
@@ -1,24 +1,24 @@
 
-// var s = prompt("Enter the word you'd like 'Coronavirus' to be replaced with");
 chrome.storage.local.get(['textInput'], function(result) {
-    // console.log("result",result);
     var elements = document.getElementsByTagName('*');
     var s = result.textInput;
-    for (var i = 0; i < elements.length; i++) {
-        var element = elements[i];
-    
-        for (var j = 0; j < element.childNodes.length; j++) {
-            var node = element.childNodes[j];
-    
-            if (node.nodeType === 3) {
-                var text = node.nodeValue;
-                var replacedText = text.replace(/coronavirus/gi, s);
-                var replacedText = replacedText.replace(/covid-19/gi, s);
-                var replacedText = replacedText.replace(/Coronavirus/gi, s);
-                var replacedText = replacedText.replace(/Covid-19/gi, s);
-    
-                if (replacedText !== text) {
-                    element.replaceChild(document.createTextNode(replacedText), node);
+    if (s !== "") {
+        for (var i = 0; i < elements.length; i++) {
+            var element = elements[i];
+        
+            for (var j = 0; j < element.childNodes.length; j++) {
+                var node = element.childNodes[j];
+        
+                if (node.nodeType === 3) {
+                    var text = node.nodeValue;
+                    var replacedText = text.replace(/coronavirus/gi, s);
+                    var replacedText = replacedText.replace(/covid-19/gi, s);
+                    var replacedText = replacedText.replace(/Coronavirus/gi, s);
+                    var replacedText = replacedText.replace(/Covid-19/gi, s);
+        
+                    if (replacedText !== text) {
+                        element.replaceChild(document.createTextNode(replacedText), node);
+                    }
                 }
             }
         }
--- popup.js
+++ popup.js
@@ -1,7 +1,9 @@
-let replaceButton = document.getElementById("replace-button");
-replaceButton.addEventListener("click", saveText);
-
+document.getElementById("replace-button").addEventListener("click", saveText);
 function saveText() {
     let customText = document.getElementById("wordInput").value;
     chrome.storage.local.set({textInput: customText});
+    chrome.tabs.getSelected(null, function(tab) {
+        var code = 'window.location.reload();';
+        chrome.tabs.executeScript(tab.id, {code: code});
+    });
 }
