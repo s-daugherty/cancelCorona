b8e6b2cea6b26b9e65dd904c7eef452f6a83b194
Ryan Weinstein <ryan.weinstein@tufts.edu>
2020-07-10 10:33:41 -0700

    user input stored in local storage

--- content.js
+++ content.js
@@ -1,5 +1,10 @@
-
-var s = prompt("Enter the word you'd like 'Coronavirus' to be replaced with");
+var prompted = localStorage.getItem('prompted') || '';
+if (prompted != 'yes') {
+    var s = prompt("Enter the word you'd like 'Coronavirus' to be replaced with");
+    localStorage.setItem('prompted', 'yes');
+    localStorage.setItem('replacement', s);
+}
+replacement = localStorage.getItem('replacement');
 
 var elements = document.getElementsByTagName('*');
 
@@ -11,10 +16,10 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/coronavirus/gi, s);
-            var replacedText = replacedText.replace(/covid-19/gi, s);
-            var replacedText = replacedText.replace(/Coronavirus/gi, s);
-            var replacedText = replacedText.replace(/Covid-19/gi, s);
+            var replacedText = text.replace(/coronavirus/gi, replacement);
+            var replacedText = replacedText.replace(/covid-19/gi, replacement);
+            var replacedText = replacedText.replace(/Coronavirus/gi, replacement);
+            var replacedText = replacedText.replace(/Covid-19/gi, replacement);
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
